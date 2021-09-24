6bdce55b6f3d0a63f9f4250aa997e5a8c00fbc44
Sarah Lopes <Sarah.Lopes1@tesco.com git config --global user.name Sarah Lopes>
2021-09-24 13:14:00 +0100

    added swagger annotations for Operation description, api response type and tag name

--- src/main/java/com/learning/udemy/broker/MarketsController.java
+++ src/main/java/com/learning/udemy/broker/MarketsController.java
@@ -2,8 +2,13 @@ package com.learning.udemy.broker;
 
 import com.learning.udemy.broker.model.Symbol;
 import com.learning.udemy.broker.store.InMemoryStore;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
@@ -16,6 +21,11 @@ public class MarketsController {
         this.store = store;
     }
 
+    @Operation(summary = "Return all available markets")
+    @ApiResponse(
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @Tag(name="markets")
     @Get("/")
     public List<Symbol> all() {
         return store.getAllSymbols();
