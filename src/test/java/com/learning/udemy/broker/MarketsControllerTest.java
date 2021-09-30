306b4d5c451b602e70b8487fb22965cfd0a385fc
Sarah Lopes <Sarah.Lopes1@tesco.com git config --global user.name Sarah Lopes>
2021-09-30 21:03:04 +0100

    updated tests to use soft assertions in MarketsControllerTest

--- src/test/java/com/learning/udemy/broker/MarketsControllerTest.java
+++ src/test/java/com/learning/udemy/broker/MarketsControllerTest.java
@@ -4,13 +4,11 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.netty.DefaultHttpClient;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 public class MarketsControllerTest {
@@ -22,9 +20,12 @@ public class MarketsControllerTest {
     @Test
     void returnsListOfMarkets() {
         final List<LinkedHashMap<String, String>> result = client.toBlocking().retrieve("/markets", List.class);
-        assertEquals(7, result.size());
-        assertThat(result)
-                .extracting(entry -> entry.get("value"))
-                .containsExactlyInAnyOrder("APPL", "AMZN", "FB", "GOOGLE", "MCSFT", "NFLX", "TSLA");
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(result.size()).isEqualTo(7);
+            softly.assertThat(result)
+                    .extracting(entry -> entry.get("value"))
+                    .containsExactlyInAnyOrder("APPL", "AMZN", "FB", "GOOGLE", "MCSFT", "NFLX", "TSLA");
+        });
     }
 }
